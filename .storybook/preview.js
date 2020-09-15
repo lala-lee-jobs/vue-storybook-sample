f39906efae3a654ab4a85fb0a47133a4e00e2950
拉拉醬 <lala.lee.goo@gmail.com>
2020-09-15 22:01:56 +0800

    parameters - global level

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,10 @@
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
+  backgrounds: {
+    values: [
+      { name: 'global-red', value: 'red' },
+      { name: 'global-green', value: 'green' },
+    ],
+  },
 }
